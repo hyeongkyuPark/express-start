632e3424569217ce676e6dad0229a585453ca4e1
parkhyeongkyu <phg0644@gmail.com>
2020-06-24 12:51:04 +0900

    express-start

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (request, response) => {
+    response.send("express start~!");
+});
+
+app.listen(port, () => {
+    
+});
